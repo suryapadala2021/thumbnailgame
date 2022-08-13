e98214a18d8e3ab0ac55862b872ffb6a223ce2fe
suryareddy <suryapadala2021@gmail.com>
2022-08-13 11:21:50 +0000

    a

--- src/Game/index.js
+++ src/Game/index.js
@@ -8,12 +8,16 @@ class game extends Component {
     active: 'FRUIT',
     matchImageId: 'b11ec8ce-35c9-4d67-a7f7-07516d0d8186',
     score: 0,
+    sec: 60,
+    gameStatus: true,
   }
 
-  TabChange = name => {
-    this.setState({
-      active: name,
-    })
+  componentDidMount() {
+    this.timerId = setInterval(this.tick, 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId)
   }
 
   getAnotherImgId = () => {
@@ -27,12 +31,36 @@ class game extends Component {
     if (unq === matchImageId) {
       const imgId = this.getAnotherImgId()
       this.setState(prev => ({score: prev.score + 1, matchImageId: imgId}))
+    } else {
+      this.setState({gameStatus: false})
+      clearInterval(this.timerId)
     }
+  }
+
+  tick = () => {
+    const {sec} = this.state
+    if (sec > 0) {
+      this.setState(prev => ({sec: prev.sec - 1}))
+    } else {
+      this.setState({gameStatus: false})
+      clearInterval(this.timerId)
+    }
+  }
+
+  TabChange = name => {
+    this.setState({
+      active: name,
+    })
+  }
+
+  reset = () => {
+    this.setState({score: 0, sec: 60, gameStatus: true})
+    this.componentDidMount()
   }
 
   render() {
     const {tabsList, imagesList} = this.props
-    const {active, matchImageId, score} = this.state
+    const {active, matchImageId, score, sec, gameStatus} = this.state
     const itemsList = imagesList.filter(obj => obj.category === active)
     const getImage = imagesList.find(each => each.id === matchImageId)
     return (
@@ -59,37 +87,65 @@ class game extends Component {
               />
             </li>
             <li>
-              <p className="score-count">0 sec</p>
+              <p className="score-count">{sec} sec</p>
             </li>
           </ul>
           <div className="game-container">
-            <div className="match-img-box">
-              <img
-                src={getImage.imageUrl}
-                alt="match"
-                className="matching-image"
-              />
-            </div>
-            <ul className="tabs-container">
-              {tabsList.map(each => (
-                <Tabs
-                  key={each.tabId}
-                  details={each}
-                  isActive={each.tabId === active}
-                  TabChange={this.TabChange}
+            {gameStatus && (
+              <>
+                <div className="match-img-box">
+                  <img
+                    src={getImage.imageUrl}
+                    alt="match"
+                    className="matching-image"
+                  />
+                </div>
+                <ul className="tabs-container">
+                  {tabsList.map(each => (
+                    <Tabs
+                      key={each.tabId}
+                      details={each}
+                      isActive={each.tabId === active}
+                      TabChange={this.TabChange}
+                    />
+                  ))}
+                </ul>
+                <ul className="thumbnail-container">
+                  {itemsList.map(obj => (
+                    <Thumbnail
+                      key={obj.id}
+                      details={obj}
+                      isActive={obj.tabId === active}
+                      isMatch={this.isMatch}
+                    />
+                  ))}
+                </ul>
+              </>
+            )}
+            {!gameStatus && (
+              <div className="Score-card">
+                <img
+                  className="trophy-img"
+                  src="https://assets.ccbp.in/frontend/react-js/match-game-trophy.png "
+                  alt="trophy"
                 />
-              ))}
-            </ul>
-            <ul className="thumbnail-container">
-              {itemsList.map(obj => (
-                <Thumbnail
-                  key={obj.id}
-                  details={obj}
-                  isActive={obj.tabId === active}
-                  isMatch={this.isMatch}
-                />
-              ))}
-            </ul>
+                <h1 className="score-display">YOUR SCORE</h1>
+                <h1 className="score-display">{score}</h1>
+                <div className="btn-box">
+                  <button
+                    type="button"
+                    className="restart-game"
+                    onClick={this.reset}
+                  >
+                    <img
+                      src="https://assets.ccbp.in/frontend/react-js/match-game-play-again-img.png"
+                      alt="reset"
+                    />
+                    PLAY AGAIN
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
